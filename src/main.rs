a2b7ef2f612d2168156390323cda753c343ba5bc
Nodo1221 <killforskill2005@gmail.com>
2026-05-17 01:48:58 +0200

    Mul seems to work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#[derive(Debug)]
 enum Token {
     Num(u64),
     Plus, Minus, Star, Slash,
 }
 
 /// Parsed
+#[derive(Debug)]
 enum Expr {
     Num(u64),
     Binary {
@@ -17,29 +19,37 @@ enum Expr {
     }
 }
 
+#[derive(Debug)]
 enum Op { Add, Subs, Mul, Div, Neg }
 
 fn parse_mul_level(mut tokens: &[Token]) -> Box<Expr> {
+    assert!(!tokens.is_empty());
+         
     let mut i = 0;
 
-    let Token::Num(first) = tokens[0] else { return; };
+    let Token::Num(first) = tokens[0] else { panic!() };
 
     let mut tree = Box::new(Expr::Num(first));
+
+    tokens = &tokens[1..];
         
     loop {
+        println!("loop: {:?}", tokens);
+
         if tokens.is_empty() {
             return tree;
         }
 
-        match tokens[1] {
-            Token::Plus | Token::Minus => {return;}
+        match tokens[0] {
+            Token::Plus | Token::Minus => {return tree;}
             Token::Star | Token::Slash => {
-                let Token::Num(another_num) = tokens[2] else {return;};
+                println!("shift");
+                let Token::Num(another_num) = tokens[1] else {return tree;};
                 // remove boxes
                 tree = Box::new(Expr::Binary { op: Box::new(Op::Mul), left: tree, right: Box::new(Expr::Num(another_num)) });
             tokens = &tokens[2..];
             }
-            _ => {return;}
+            _ => {return tree;}
         }
 
         i += 1;
@@ -49,6 +59,10 @@ fn parse_mul_level(mut tokens: &[Token]) -> Box<Expr> {
 fn main() {
     use Token::*;
 
-    let lexxed = vec![Num(2), Plus, Num(3), Star, Num(2)];
+    let lexxed = vec![Num(2), Star, Num(3), Star, Num(4)];
+
+    let tree = parse_mul_level(&lexxed);
+
+    println!("{:?}", *tree);
 
 }
