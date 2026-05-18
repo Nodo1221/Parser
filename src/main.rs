2438b1c10bde1099caec80c9d072833b3428f557
Nodo1221 <killforskill2005@gmail.com>
2026-05-18 19:55:01 +0200

    Cleanup checkpoint, not working for sequential addition

--- src/main.rs
+++ src/main.rs
@@ -59,47 +59,32 @@ fn parse_mul_level(mut tokens: &[Token]) -> (Box<Expr>, &[Token]) {
 
 fn parse_add_level(mut tokens: &[Token]) -> (Box<Expr>, &[Token]) {
     assert!(!tokens.is_empty());
-         
-    let mut i = 0;
 
-
-    // let Token::Num(first) = tokens[0] else { panic!() };
-
+    // Parse level higher
     let (mut tree, mut tokens) = parse_mul_level(tokens);
-
-    // tokens = &tokens[1..];
         
-    loop {
+    while !tokens.is_empty() {        
         println!("add loop: {:?}", tokens);
+        
+        assert!(matches!(tokens[0], Token::Plus | Token::Minus), "All higher level tokens should've been parsed. Encountered: {:?}", tokens[0]);
 
-        if tokens.is_empty() {
-            return (tree, tokens);
-        }
-
-        let Token::Plus = tokens[0] else { panic!("not a plus mate {:?}", tokens[0]) };
         tokens = &tokens[1..];
 
         let Token::Num(number) = tokens[0] else { panic!("not a number but: {:?}", tokens[0]) };
 
-        // look ahead:
+        // Look ahead
         match tokens[1] {
-            Token::Plus | Token::Minus => {
-                //continue normally
-            }
+            // Defer to level higher
             Token::Star | Token::Slash => {
-                // let newtree: Vec<&[Token]> = &tokens[0..1].collect();
-                // let newtree = vec![tokens[0].clone()];
-
-
                 (tree, tokens) = parse_mul_level(&tokens[2..]);
 
                 tree = Box::new(Expr::Binary { op: Op::Add, left: Box::new(Expr::Num(number)), right: tree });
 
-                // newtree.push(tokens);
-
                 println!("after lookahead mul parsing: {:?}", tokens);
             }
-            _ => unreachable!("{:?}", tokens[2]),
+
+            // Continue normally
+            _ => {}
 
         }
 
@@ -109,25 +94,22 @@ fn parse_add_level(mut tokens: &[Token]) -> (Box<Expr>, &[Token]) {
                 tree = Box::new(Expr::Binary { op: Op::Add, left: tree, right: Box::new(Expr::Num(another_num)) });
                 tokens = &tokens[2..];
             }
-            // Token::Star | Token::Slash => {
-            //     println!("shift");
-            //     let Token::Num(another_num) = tokens[1] else {return tree;};
-            //     // remove boxes
-            //     tree = Box::new(Expr::Binary { op: Op::Mul, left: tree, right: Box::new(Expr::Num(another_num)) });
-            // tokens = &tokens[2..];
-            // }
             _ => {return (tree, tokens);}
         }
-
-        i += 1;
     }
+    (tree, tokens)
 }
+
+// 2 + 3 * 2
+// 2 + 3 + 4
 
 fn main() {
     use Token::*;
 
     // let lexxed = vec![Num(2), Star, Num(3), Star, Num(4)];
-    let lexxed = vec![Num(2), Plus, Num(3), Star, Num(4), Plus, Num(1)];
+    // 2 + 3 * 4 + 1
+    // let lexxed = vec![Num(2), Plus, Num(3), Star, Num(4), Plus, Num(1)];
+    let lexxed = vec![Num(2), Plus, Num(3), Plus, Num(4)];
     // let lexxed = vec![Num(2)];
 
     let (tree, _) = parse_add_level(&lexxed);
