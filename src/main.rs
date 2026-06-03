a6b3192d9c6da9f5c589722d809dddf64ff8ebfd
Nodo1221 <killforskill2005@gmail.com>
2026-06-03 02:29:39 +0200

    Lexer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[derive(Debug)]
+#[allow(dead_code)]
 enum ParseError {
     UnexpectedToken(Token)
 }
@@ -7,6 +8,7 @@ enum ParseError {
 enum Token {
     Num(u64),
     Plus, Minus, Star, Slash,
+    Lpar, Rpar,
 }
 
 impl TryFrom<&Token> for Op {
@@ -24,6 +26,7 @@ impl TryFrom<&Token> for Op {
 }
 
 #[derive(Debug)]
+#[allow(dead_code)]
 enum Expr {
     Num(u64),
     Binary {
@@ -56,8 +59,14 @@ fn parse(tokens: &[Token], level: usize) -> (Box<Expr>, &[Token]) {
                 Token::Num(n) => (Box::new(Expr::Unary { op: Op::Neg, left: Box::new(Expr::Num(n)) }), &tokens[2..]),
                 _ => unreachable!("Expected Num after Minus"),
             },
-            // TODO: handle parenthesis here (match lparen, restart parsing from the lowest prededence, then look for right paren)
-            _ => unreachable!("Expected Num or Minus"),
+
+            // Handle parenthesis (match lpar, restart parsing from the lowest prededence, then look for rpar)
+            Token::Lpar => {
+                let (tree, tokens) = parse(&tokens[1..], 0);
+                assert!(matches!(tokens[0], Token::Rpar), "Expected ')' Rpar");
+                (tree, &tokens[1..])
+            }
+            _ => unreachable!("Expected Num or Minus, tokens: {:?}", tokens),
         };
 
     }
@@ -90,14 +99,57 @@ fn parse_expr(tokens: &[Token]) -> Box<Expr> {
     tree
 }
 
+fn lex(input: &str) -> Vec<Token> {
+    let mut chars = input.chars().peekable();
+    let mut tokens = Vec::new();
+
+    while let Some(&c) = chars.peek() {
+        match c {
+            ' ' => { chars.next(); }
+            '0'..='9' => {
+                let mut n = 0u64;
+                while let Some(d) = chars.next_if(|c| c.is_ascii_digit()) {
+                    n = n * 10 + d.to_digit(10).unwrap() as u64;
+                }
+                tokens.push(Token::Num(n))
+            }
+            _ => {
+                let token = match c {
+                    '+' => Token::Plus,
+                    '-' => Token::Minus,
+                    '*' => Token::Star,
+                    '/' => Token::Slash,
+                    '(' => Token::Lpar,
+                    ')' => Token::Rpar,
+                    _ => panic!("Unexpected char: {c}"),
+                };
+                tokens.push(token);
+                chars.next();
+            }
+        }
+    }
+
+    return tokens;
+}
+
 fn main() {
-    use Token::*;
+    let in1 = "(2 + -3) * 4";
+    let in2 = "2 * 3 * -4";
+    let in3 = "2 + 3 * 4 + 1";
 
-    // let lexxed = vec![Num(2), Plus, Num(3), Plus, Num(4)];
-    // let lexxed = vec![Num(2), Star, Num(3), Star, Num(4)];
-    let lexxed = vec![Num(2), Plus, Num(3), Star, Num(4), Plus, Num(1)];
+    let lex1 = lex(in1);
+    let lex2 = lex(in2);
+    let lex3 = lex(in3);
 
-    let tree = parse_expr(&lexxed);
-    
-    println!("{:?}", tree);
+    let tree1 = parse_expr(&lex1);
+    let tree2 = parse_expr(&lex2);
+    let tree3 = parse_expr(&lex3);
+
+    println!("1: {in1:<15} {:?}", lex1);
+    println!("2: {in2:<15} {:?}", lex2);
+    println!("3: {in3:<15} {:?}", lex3);
+    println!();
+    println!("1: {:?}", tree1);
+    println!("2: {:?}", tree2);
+    println!("3: {:?}", tree3);
 }
